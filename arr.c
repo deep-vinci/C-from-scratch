e06b866dd31ae2779bccb4bb0b82bd64980de39c
Deepak <deepakworkpc@gmail.com>
2025-01-30 11:05:17 +0530

    fix: remove the len_arr method as the array is getting decayed when passed as a parameter to the len_arr method

--- arr.c
+++ arr.c
@@ -2,14 +2,7 @@
 
 int arr_len(int arr[])
 {
-    for (size_t i = 0; i < 7; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    line_break();
-    printf("%zu", sizeof(arr));
-    line_break();
-    return sizeof(arr);
+    return sizeof(arr) / sizeof(*arr);
 }
 
 void line_break()
@@ -22,8 +15,8 @@ void main(int argc, char *argv[])
     int a[] = {1, 2, 3, 4, 5, 6, 7};
     int b = 2;
 
-    int len_of_arr0 = arr_len(a);
-    int len_of_arr1 = sizeof(a);
+    int len_of_arr0 = sizeof(a) / sizeof(*a);
+    int len_of_arr1 = sizeof(a) / sizeof(int);
     int len_of_arr2 = sizeof(a) / sizeof(a[0]);
     printf("%lu %lu %d %d %d %d", sizeof(b), sizeof(a), len_of_arr0, len_of_arr1, len_of_arr2, *a);
     line_break();
