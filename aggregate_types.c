3d7d029c4a61b30bb945e2ef49b1429df7bae1df
Deepak <deepakworkpc@gmail.com>
2025-02-01 01:12:45 +0530

    docs: add the tm type doc

--- aggregate_types.c
+++ aggregate_types.c
@@ -23,5 +23,27 @@ int main(int argc, char *argv[])
         long tv_nsec;
     }
 
+    struct tm
+    {
+        int tm_sec;   // Seconds after the minute
+        int tm_min;   // Minutes after the hour
+        int tm_hour;  // Hours since midnight
+        int tm_mday;  // Day of the month
+        int tm_mon;   // Months since January
+        int tm_year;  // Years since 1900
+        int tm_wday;  // Days since Sunday
+        int tm_yday;  // Days since January
+        int tm_isdst; // Daylight Saving Time flag
+    };
+
+    struct nestedStruct
+    {
+        char *name;
+        struct stardate
+        {
+            struct tm date;
+            struct timespec precision;
+        }
+    };
     printf("%s", name);
 }
