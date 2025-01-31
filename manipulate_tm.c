b707dba8bb8fe51e2357feab7fd13a5444381ac9
Deepak <deepakworkpc@gmail.com>
2025-02-01 01:32:16 +0530

    feat: add macro for DAYS_BEFORE, useful for calculating days past from 1 JAN, you do it by add the DAYS_BEFORE[tm_mon] + tm_mday-1

--- manipulate_tm.c
+++ manipulate_tm.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <time.h>
 
+#define DAYS_BEFORE (int[12]{0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334})
+
+struct tm time_set_yday(struct tm t)
+{
+    t.tm_yday =
+}
+
 int main(int argc, char *argv[])
 {
+
+    struct tm
+    {
+        int tm_sec;   // Seconds after the minute
+        int tm_min;   // Minutes after the hour
+        int tm_hour;  // Hours since midnight
+        int tm_mday;  // Day of the month
+        int tm_mon;   // Months since January
+        int tm_year;  // Years since 1900
+        int tm_wday;  // Days since Sunday
+        int tm_yday;  // Days since January
+        int tm_isdst; // Daylight Saving Time flag
+    };
+
     struct tm today = {
         .tm_year = 2025 - 1990,
         .tm_mon = 1 - 1,
@@ -13,4 +34,5 @@ int main(int argc, char *argv[])
     };
 
     printf("this year is %d, next year will be %d\n", today.tm_year + 1990, today.tm_year + 1990 + 1);
+    // today
 }
