bde582c049834c0a8c5923fdac88f662b8eb29d4
Deepak <deepakworkpc@gmail.com>
2025-02-01 02:09:21 +0530

    feat: testing out the claim, that inner structs got same scope, it doesnt

--- aggregate_types.c
+++ aggregate_types.c
@@ -2,48 +2,53 @@
 
 int main(int argc, char *argv[])
 {
-    struct birdStruct
-    {
-        char *jay;
-        char *magpie;
-        char *raven;
-        char *chough;
-    };
+    // struct birdStruct
+    // {
+    //     char *jay;
+    //     char *magpie;
+    //     char *raven;
+    //     char *chough;
+    // };
 
-    struct birdStruct name = {
-        .chough = "Joe",
-        .jay = "jayfr",
-        .magpie = "maggie",
-        .raven = "ravan",
-    };
+    // struct birdStruct name = {
+    //     .chough = "Joe",
+    //     .jay = "jayfr",
+    //     .magpie = "maggie",
+    //     .raven = "ravan",
+    // };
 
-    struct timeSpec
-    {
-        time_t tv_sec;
-        long tv_nsec;
-    }
+    // struct timeSpec
+    // {
+    //     time_t tv_sec;
+    //     long tv_nsec;
+    // }
 
-    struct tm
-    {
-        int tm_sec;   // Seconds after the minute
-        int tm_min;   // Minutes after the hour
-        int tm_hour;  // Hours since midnight
-        int tm_mday;  // Day of the month
-        int tm_mon;   // Months since January
-        int tm_year;  // Years since 1900
-        int tm_wday;  // Days since Sunday
-        int tm_yday;  // Days since January
-        int tm_isdst; // Daylight Saving Time flag
-    };
+    // struct tm
+    // {
+    //     int tm_sec;   // Seconds after the minute
+    //     int tm_min;   // Minutes after the hour
+    //     int tm_hour;  // Hours since midnight
+    //     int tm_mday;  // Day of the month
+    //     int tm_mon;   // Months since January
+    //     int tm_year;  // Years since 1900
+    //     int tm_wday;  // Days since Sunday
+    //     int tm_yday;  // Days since January
+    //     int tm_isdst; // Daylight Saving Time flag
+    // };
 
     struct nestedStruct
     {
         char *name;
         struct stardate
         {
-            struct tm date;
-            struct timespec precision;
-        }
+            int mock;
+            // struct tm date;
+            // struct timespec precision;
+        };
     };
-    printf("%s", name);
+
+    struct nestedStruct mocked = {
+        .name = "daslij",
+    };
+    printf("%s", mocked.name);
 }
