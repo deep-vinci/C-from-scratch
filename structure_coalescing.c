7ea07603ee5e4eab0958d82a0aabc9a7479d2703
Deepak <deepakworkpc@gmail.com>
2025-02-01 10:15:15 +0530

    feat: add uncoalesced and coalesced data

--- structure_coalescing.c
+++ structure_coalescing.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
 // uncoalesced structure
-struct data
+struct uncoalesced
 {
     char a;
     int b;
     char c;
 };
 
+// coalesced structure
+#pragma pack(1)
+
+struct coalesced
+{
+    char b;
+    char c;
+    int a;
+};
+
+#pragma pack()
+
 int main(int argc, char *argv[])
 {
-    struct data dataOne = {
+    struct uncoalesced dataUncoalesced = {
         .a = 'A',
         .b = 21,
         .c = 'B',
     };
 
-    printf("%ld", sizeof(dataOne));
+    struct coalesced dataCoalesced = {
+        .a = 2,
+        .b = 'a',
+        .c = 'c',
+    };
+
+    printf("%ld\n", sizeof(dataUncoalesced));
+    printf("%ld\n", sizeof(dataCoalesced));
 }
